51e2b538e375aff4fcabd2c75954cc5f9f3377a0
Ibrahim Yakubu <ibrahimy437@gmail.com>
2023-10-25 13:52:48 +0100

    add routing feature

--- src/App.js
+++ src/App.js
@@ -8,14 +8,17 @@ import "./App.css";
 
 // layouts and pages
 import RootLayout from "./layouts/RootLayout";
-import Dashboard from "./pages/Dashboard";
+import Dashboard, { tasksLoader } from "./pages/Dashboard";
 import Profile from "./pages/Profile";
+import LandingPage from "./pages/LandingPage";
+import Lands from "./pages/Lands";
 
 // router and routes
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
-      <Route index element={<Dashboard />} />
+      <Route index element={<Lands />} loader={tasksLoader} />
+      <Route path="dashboard" element={<Dashboard />} loader={tasksLoader} />
       <Route path="profile" element={<Profile />} />
     </Route>
   )
